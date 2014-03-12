9e5a60d74ec4feb1ccbb674bef84c2cc8a7f46f1
Christian Flinker Sandbeck <info@sandbeck.net>
2014-03-12 12:00:14 +0100

    The progress update thread now updates every 250ms and resets the media player to beginning of the song when finished.

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends ActionBarActivity {
     Button stopb;
     SeekBar sb;
     MediaPlayer mp;
-
     Runnable r;
 
     @Override
@@ -34,15 +33,30 @@ public class MainActivity extends ActionBarActivity {
         stopb = (Button) findViewById(R.id.stopb);
         sb = (SeekBar) findViewById(R.id.sb);
 
-
+        r = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (!mp.isPlaying()) {
+                        mp.seekTo(0);
+                    }
+                    sb.setProgress(mp.getCurrentPosition());
+                    try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException e) {
+                        Log.e("MedaiPlayerThread", "ProgressFAIL", e);
+                    }
+                }
+            }
+        };
 
         playb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mp==null){
+                if (mp == null) {
                     mp = MediaPlayer.create(getBaseContext(), Uri.parse("http://gototen.dk/wp-content/uploads/2013/12/dont-mess-with-my-man.mp3"));
                     sb.setMax(mp.getDuration());
-                    Thread t =new Thread(r);
+                    Thread t = new Thread(r);
                     t.start();
                 }
                 mp.start();
@@ -51,70 +65,47 @@ public class MainActivity extends ActionBarActivity {
         pauseb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mp==null){return;}
-                if(!mp.isPlaying()){
+                if (mp == null) {
+                    return;
+                }
+                if (!mp.isPlaying()) {
                     mp.start();
 
-                }
-                else
-                {
+                } else {
                     mp.pause();
-
                 }
             }
         });
         stopb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mp==null){return;}
-                if(mp.isPlaying())
-                {
+                if (mp == null) {
+                    return;
+                }
+                if (mp.isPlaying()) {
                     mp.pause();
                     mp.seekTo(0);
                 }
-
             }
         });
 
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if(b){
+                if (b) {
                     mp.seekTo(i);
                 }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                //Should do nothing
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                //Should do nothing
             }
         });
-        r = new Runnable() {
-            @Override
-            public void run() {
-                while(true){
-                    sb.setProgress(mp.getCurrentPosition());
-                    try {
-                        Thread.sleep(1000);
-
-                    } catch (InterruptedException e) {
-                        Log.e("MedaiPlayerThread", "ProgressFAIL",e);
-
-                    }
-
-                }
-            }
-
-
-        };
-
-
-
-
-
-    }}
+    }
+}
